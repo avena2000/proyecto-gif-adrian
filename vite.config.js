76f8488818465e98aabb4640ffe34bcf29294c27
Avn2000 <avn2000inc@gmail.com>
2023-12-04 10:34:15 -0600

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://avena2000.github.io/proyecto-gif-adrian/'
 })
